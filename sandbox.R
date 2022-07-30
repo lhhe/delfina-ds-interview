945f996b7e9a28017717afdcf7a3931f5102cc35
Leo <hernandez.leo@gmail.com>
2022-07-30 08:45:36 -0700

    adding comments

--- sandbox.R
+++ sandbox.R
@@ -1,4 +1,9 @@
-data_url <- "~/R/delfina-ds-interview/data/birthdata2020.csv"
+library(tidyverse)
 
+# Import data
+# If you are testing this outside my repo, please change the url
+data_url <- "~/R/delfina-ds-interview/data/birthdata2020.csv"
 birth_data <- read.csv(data_url, header = TRUE)
+
+# Explore data
 head(birth_data)
