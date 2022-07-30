d58d697cf37f13c963d2f1cc4d1f46d989c3d8d0
Leo <hernandez.leo@gmail.com>
2022-07-30 11:07:02 -0700

    data dictionary

--- sandbox.R
+++ sandbox.R
@@ -6,4 +6,18 @@ data_url <- "~/R/delfina-ds-interview/data/birthdata2020.csv"
 birth_data <- read.csv(data_url, header = TRUE)
 
 # Explore data
+
 head(birth_data)
+
+cols <- colnames(birth_data)
+
+typeof(cols)
+length(cols)
+cols[1]
+
+# Create a data dictionary to better understand the variables
+data_description <- data.frame(var_codes = cols)
+write.csv(data_description,
+    file =
+        "~/R/delfina-ds-interview/data/data_dictionary.csv"
+)
