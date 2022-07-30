7d74ad3350c5f3226193b40da836df36c04e915a
Leo <hernandez.leo@gmail.com>
2022-07-30 13:39:35 -0700

    updating notebook

--- my_files/sandbox.R
+++ my_files/sandbox.R
@@ -21,3 +21,19 @@ write.csv(data_description,
     file =
         "~/R/delfina-ds-interview/data/data_dictionary.csv"
 )
+
+
+# Q1 proportion of births by gestational age
+# I will use COMBGEST as the metric
+
+birth_data %>% 
+  filter(COMBGEST<99) %>% 
+  mutate(is_preterm = case_when(
+    COMBGEST < 37 ~ "Yes",
+    TRUE ~ "No"
+  )) %>% 
+  group_by(is_preterm) %>% 
+  summarise(n_cases = n()) %>%
+  #mutate(percentage = 100* round(n_cases / sum(n_cases), 3))
+  mutate(percentage = 100* (n_cases / sum(n_cases)))
+
